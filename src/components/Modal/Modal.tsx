5ad6337706e51aa717b2cba27b5d55ec288bad47
Geeth Wishkamal <41125548+geethwish@users.noreply.github.com>
2023-08-10 01:35:48 +0530

    product details popup modal ui developments

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from "react";
+import React, { FC, ReactElement, memo } from "react";
 import { styled } from "@mui/material/styles";
 import {
   Button,
@@ -10,8 +10,6 @@ import {
   Typography,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
-import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 
 import styles from "./Modal.module.scss";
 
@@ -34,6 +32,9 @@ interface ModalProps {
   open: boolean;
   headerType?: "image" | "text";
   header: string | ReactElement;
+  footer: ReactElement;
+  children?: React.ReactNode;
+  onClose: () => void;
 }
 
 function BootstrapDialogTitle(props: DialogTitleProps) {
@@ -42,70 +43,46 @@ function BootstrapDialogTitle(props: DialogTitleProps) {
   return (
     <DialogTitle sx={{ m: 0, p: 0 }} {...other}>
       {children}
-      {onClose ? (
-        <IconButton
-          aria-label="close"
-          onClick={onClose}
-          className={styles.iconButton}
-          color="primary"
-        >
-          <CloseIcon />
-        </IconButton>
-      ) : null}
+
+      <IconButton
+        aria-label="close"
+        onClick={() => onClose()}
+        className={styles.iconButton}
+        color="primary"
+      >
+        <CloseIcon />
+      </IconButton>
     </DialogTitle>
   );
 }
 
-const Modal: FC<ModalProps> = ({ open, header, headerType }) => {
-  const handleClickOpen = () => {};
-  const handleClose = () => {};
+const Modal: FC<ModalProps> = ({
+  open,
+  header,
+  headerType,
+  footer,
+  children,
+  onClose,
+}) => {
   return (
     <BootstrapDialog
-      onClose={handleClose}
+      onClose={onClose}
       aria-labelledby="customized-dialog-title"
       open={open}
       className={styles.modalContainer}
     >
-      <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
-        {header}
-      </BootstrapDialogTitle>
-      <DialogContent dividers className={styles.contentWrapper}>
-        <Typography gutterBottom variant="h3" className={styles.title}>
-          Salad Bundle
-        </Typography>
-        <Typography gutterBottom>
-          A perfect healthy & delicious meal. Choose from one of our favourite
-          salads, add a drink and snack for just £14.99
-        </Typography>
-        <Typography gutterBottom>
-          Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-          magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-          ullamcorper nulla non metus auctor fringilla.
-        </Typography>
-      </DialogContent>
-      <DialogActions className={styles.actionWrapper}>
-        <div>
-          <IconButton aria-label="close" color="primary">
-            <RemoveCircleOutlineIcon />
-          </IconButton>
-          1
-          <IconButton aria-label="close" color="primary">
-            <AddCircleOutlineOutlinedIcon />
-          </IconButton>
-        </div>
-        <Button
-          autoFocus
-          onClick={handleClose}
-          fullWidth
-          variant="contained"
-          className="custom-primary-button"
-          disabled={true}
-        >
-          Add for £14.99
-        </Button>
-      </DialogActions>
+      <div className={styles.modalContentContainer}>
+        <BootstrapDialogTitle id="customized-dialog-title" onClose={onClose}>
+          {header}
+        </BootstrapDialogTitle>
+        <DialogContent dividers className={styles.contentWrapper}>
+          {children}
+        </DialogContent>
+      </div>
+
+      {footer}
     </BootstrapDialog>
   );
 };
 
-export default Modal;
+export default memo(Modal);
--- src/features/products/components/CategoryProductsList/CategoryProductsList.tsx
+++ src/features/products/components/CategoryProductsList/CategoryProductsList.tsx
@@ -1,10 +1,26 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import ProductCard from "../ProductCard/ProductCard";
 import styles from "./CategoryProductsList.module.scss";
-import { Typography } from "@mui/material";
+import {
+  Button,
+  Checkbox,
+  DialogActions,
+  Divider,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  Radio,
+  RadioGroup,
+  Typography,
+} from "@mui/material";
 import { SingleProductType } from "../../slices/products.slice";
 import { Waypoint } from "react-waypoint";
 import Modal from "../../../../components/Modal/Modal";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import { useAppSelector } from "../../../../app/hooks";
+import { loginStatus } from "../../../../app/slices/login.slice";
 
 interface CategoryProductsListProps {
   id: string;
@@ -18,15 +34,26 @@ const CategoryProductsList: FC<CategoryProductsListProps> = ({
   id,
   onFocusElement,
 }) => {
-  const [showModal, setShowModal] = useState(false);
+  const isAuthorized = useAppSelector(loginStatus);
+  const [open, setOpen] = useState(false);
 
   const handleElementFocus = () => {
     onFocusElement(title);
   };
 
   const handleClickCard = () => {
-    setShowModal(true);
+    setOpen((prev) => !prev);
   };
+
+  const handleClose = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const handleSubmit = () => {};
+
+  useEffect(() => {
+    console.log(open);
+  }, [open]);
 
   return (
     <Waypoint
@@ -53,18 +80,362 @@ const CategoryProductsList: FC<CategoryProductsListProps> = ({
               <ProductCard data={data} key={index} />
             ))}
         </div>
-        <Modal
-          open={showModal}
-          headerType="image"
-          header={
-            <div className={styles.modalHeader}>
-              <img
-                src={require("../../../../asset/images/prducts/bg.webp")}
-                alt={title}
-              />
+
+        {open && (
+          <Modal
+            onClose={handleClose}
+            open={open}
+            headerType="image"
+            header={
+              <div className={styles.modalHeader}>
+                <img
+                  src={require("../../../../asset/images/prducts/bg.webp")}
+                  alt={title}
+                />
+              </div>
+            }
+            footer={
+              <DialogActions className={styles.actionWrapper}>
+                <div className={styles.incrementSection}>
+                  <div className={styles.incrementButtonsWrapper}>
+                    <IconButton
+                      aria-label="close"
+                      color="primary"
+                      disabled={true}
+                    >
+                      <RemoveCircleOutlineIcon className={styles.size24} />
+                    </IconButton>
+                    <span className={styles.number}>1</span>
+
+                    <IconButton
+                      aria-label="close"
+                      color="primary"
+                      disabled={true}
+                    >
+                      <AddCircleOutlineOutlinedIcon className={styles.size24} />
+                    </IconButton>
+                  </div>
+                </div>
+                <Button
+                  autoFocus
+                  onClick={handleSubmit}
+                  fullWidth
+                  variant="contained"
+                  className="custom-primary-button"
+                  disabled={true}
+                >
+                  Add for £14.99
+                </Button>
+              </DialogActions>
+            }
+          >
+            <div>
+              <Typography gutterBottom variant="h3" className={styles.title}>
+                Salad Bundle
+              </Typography>
+              <Typography gutterBottom>
+                A perfect healthy & delicious meal. Choose from one of our
+                favourite salads, add a drink and snack for just
+              </Typography>
+              <span>£14.99</span>
+              <Divider className={styles.dividerSpace} />
+              <p className={styles.subtitle}>Contains No known allergens.</p>
+              <Typography gutterBottom>
+                Questions about allergens, ingredients or cooking methods?
+              </Typography>
+              <Button
+                variant="text"
+                className={`custom-primary-link ${styles.customButton}`}
+              >
+                Please contact the restaurant.
+              </Button>
+              <Divider className={styles.dividerSpace} />
+
+              <p className={styles.subtitle1}>Choose your salad</p>
+              <small className={styles.requiredText}>Required</small>
+
+              <div>
+                <FormControl className={styles.customRadioGroup}>
+                  <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue="chicken"
+                    name="radio-buttons-group"
+                    className={styles.customRadioGroup}
+                  >
+                    <div className={styles.radioButtonWrapper}>
+                      <FormControlLabel
+                        className={styles.customRadioGroupLabel}
+                        value="Chipotle"
+                        control={<Radio disabled={true} />}
+                        label={
+                          <>
+                            <p
+                              className={`${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              Chipotle chicken{" "}
+                            </p>
+                            <Typography
+                              gutterBottom
+                              className={`${styles.description} ${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              Contains eggs, milk ( including lactose), mustard,
+                              sulphur dioxide/sulphites. 617 Kcal
+                            </Typography>
+                          </>
+                        }
+                      />
+
+                      <FormControlLabel
+                        className={styles.customRadioGroupLabel}
+                        value="Chipotle"
+                        control={<Radio disabled={true} />}
+                        label={
+                          <>
+                            <p
+                              className={`${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              Avocado Caesar Chicken Salad
+                            </p>
+                            <Typography
+                              gutterBottom
+                              className={`${styles.description} ${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              Contains eggs, gluten, milk (including lactose),
+                              soybeans. 514 kcal
+                            </Typography>
+                          </>
+                        }
+                      />
+
+                      <FormControlLabel
+                        className={styles.customRadioGroupLabel}
+                        value="Chipotle"
+                        control={<Radio disabled={true} />}
+                        label={
+                          <>
+                            <p
+                              className={`${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              Halloumi Roasted Veg{" "}
+                            </p>
+                            <Typography
+                              gutterBottom
+                              className={`${styles.description} ${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              Contains milk (including lactose), mustard,
+                              sulphur dioxide/sulphites. 531 kcal
+                            </Typography>
+                          </>
+                        }
+                      />
+                    </div>
+                  </RadioGroup>
+                </FormControl>
+              </div>
+
+              <p className={`${styles.subtitle1} ${styles.margin16}`}>
+                Choose your salad
+              </p>
+
+              <div>
+                <FormGroup className={styles.customRadioGroup}>
+                  <FormControlLabel
+                    className={`customCheckboxLabel`}
+                    control={
+                      <Checkbox disabled={true} defaultChecked={false} />
+                    }
+                    label={
+                      <>
+                        <div className={styles.checkBoxLabelContent}>
+                          <div className={styles.checkboxLabel}>
+                            <p
+                              className={`${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              chicken breast{" "}
+                            </p>
+                            <Typography
+                              gutterBottom
+                              className={`${styles.checkboxDescription} ${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              No known allergens. 93 kcal
+                            </Typography>
+                          </div>
+                          <div
+                            className={`${styles.priceTag} ${
+                              isAuthorized === "unauthorized"
+                                ? styles.disabled
+                                : ""
+                            }`}
+                          >
+                            +£2.39
+                          </div>
+                        </div>
+                      </>
+                    }
+                  />
+                  <FormControlLabel
+                    className={`customCheckboxLabel`}
+                    control={
+                      <Checkbox disabled={true} defaultChecked={false} />
+                    }
+                    label={
+                      <>
+                        <div className={styles.checkBoxLabelContent}>
+                          <div className={styles.checkboxLabel}>
+                            <p
+                              className={`${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              grilled chicken thigh{" "}
+                            </p>
+                            <Typography
+                              gutterBottom
+                              className={`${styles.checkboxDescription} ${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              No known allergens. 126 kcal
+                            </Typography>
+                          </div>
+                          <div
+                            className={`${styles.priceTag} ${
+                              isAuthorized === "unauthorized"
+                                ? styles.disabled
+                                : ""
+                            }`}
+                          >
+                            +£2.39
+                          </div>
+                        </div>
+                      </>
+                    }
+                  />
+                  <FormControlLabel
+                    className={`customCheckboxLabel`}
+                    control={
+                      <Checkbox disabled={true} defaultChecked={false} />
+                    }
+                    label={
+                      <>
+                        <div className={styles.checkBoxLabelContent}>
+                          <div className={styles.checkboxLabel}>
+                            <p
+                              className={`${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              shawarma chicken{" "}
+                            </p>
+                            <Typography
+                              gutterBottom
+                              className={`${styles.checkboxDescription} ${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              No known allergens. 99 kcal
+                            </Typography>
+                          </div>
+                          <div
+                            className={`${styles.priceTag} ${
+                              isAuthorized === "unauthorized"
+                                ? styles.disabled
+                                : ""
+                            }`}
+                          >
+                            +£2.49
+                          </div>
+                        </div>
+                      </>
+                    }
+                  />
+                  <FormControlLabel
+                    className={`customCheckboxLabel`}
+                    control={
+                      <Checkbox disabled={true} defaultChecked={false} />
+                    }
+                    label={
+                      <>
+                        <div className={styles.checkBoxLabelContent}>
+                          <div className={styles.checkboxLabel}>
+                            <p
+                              className={`${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              vegan chicken{" "}
+                            </p>
+                            <Typography
+                              gutterBottom
+                              className={`${styles.checkboxDescription} ${
+                                isAuthorized === "unauthorized"
+                                  ? styles.disabled
+                                  : ""
+                              }`}
+                            >
+                              Contains soybeans. 82 kcal
+                            </Typography>
+                          </div>
+                          <div
+                            className={`${styles.priceTag} ${
+                              isAuthorized === "unauthorized"
+                                ? styles.disabled
+                                : ""
+                            }`}
+                          >
+                            +£2.79
+                          </div>
+                        </div>
+                      </>
+                    }
+                  />
+                </FormGroup>
+              </div>
             </div>
-          }
-        />
+          </Modal>
+        )}
       </div>
     </Waypoint>
   );
